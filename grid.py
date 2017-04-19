outsourced loading of training and test data BUGFIX, csv was built in the wrong order

grid.py
@@ -13,35 +13,39 @@ from random import randint
 
 
 def get_grid():
-    similarity_dict = [{'name': 'cosine',
-                        'user_based': False
-                        },
+    similarity_dict = [
+                        # {'name': 'cosine',
+                        # 'user_based': False
+                        # },
                        {'name': 'cosine',
                         'user_based': True
                         },
-                       {'name': 'pearson',
-                        'user_based': False
-                        },
-                       {'name': 'pearson',
-                        'user_based': True
-                        },
-                       {'name': 'pearson_baseline',
-                        'user_based': False
-                        },
-                       {'name': 'pearson_baseline',
-                        'user_based': True
-                        }
+                       # {'name': 'pearson',
+                       #  'user_based': False
+                       #  },
+                       # {'name': 'pearson',
+                       #  'user_based': True
+                       #  },
+                       # {'name': 'pearson_baseline',
+                       #  'user_based': False
+                       #  },
+                       # {'name': 'pearson_baseline',
+                       #  'user_based': True
+                       #  }
                        ]
 
-    knnbasic_param_grid = {'k': range(20, 50, 5),
-                           'min_k': (1, 20, 5),
+    knnbasic_param_grid = {
+                          'k': range(20, 50, 5),
+                           # 'min_k': (1, 20, 5),
                            'sim_options': similarity_dict
                            }
 
-    svd_param_grid = {'n_epochs': range(1, 10, 2),
-                      'lr_all': np.arange(0.001, 0.01, 0.003),
+    svd_param_grid = {
+                      'n_epochs': range(1, 10, 2),
+                      # 'lr_all': np.arange(0.001, 0.01, 0.003),
                       # 'biased': [True, False],
-                      'reg_all': np.arange(0.1, 0.8, 0.2), }
+                      # 'reg_all': np.arange(0.1, 0.8, 0.2),
+                      }
 
     knnmeans_param_grid = {}
 
@@ -53,7 +57,7 @@ def get_grid():
 
     algo_dict = {
                     KNNBasic: knnbasic_param_grid,
-                    #SVD: svd_param_grid,
+                    SVD: svd_param_grid,
                     # KNNWithMeans: knnmeans_param_grid,
                     # NMF: nmf_param_grid,
                     # CoClustering: coclust_param_grid,